2d6a0a9545c8068c62edba36c22432a1c4bbef74
LuisMejiasR <luisme.1993@gmail.com>
2022-07-07 17:48:46 -0400

    Inicia React

---
+++ REACT/aplicacion1/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+//formato de funcion básica
+const App = () => {
+    return (
+        <div className="container mt-4">
+            <div className="row">
+                <div className="col-8">
+                    <h1>Lista</h1>
+                </div>
+                <div className="col">
+                    <h1>Formulario</h1>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+//para exportar la funcion, tiene el mismo nombre
+export default App;
+
